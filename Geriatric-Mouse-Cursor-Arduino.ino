6c2bc9f59d048590f80cd020bebeed66ecbca933
Leighzer <bwc_nothgiel@live.com>
2024-10-17 22:45:23 -0400

    Cleanup code

--- Geriatric-Mouse-Cursor-Arduino.ino
+++ Geriatric-Mouse-Cursor-Arduino.ino
@@ -1,19 +1,16 @@
-#define buzzer 12
-
 #include <Mouse.h>
 
-int x = 0;
-int y = 0;
+const int delayMilliseconds = 50;
+const int maxMovementPerTick = 15;
 
 void setup() {
-  // put your setup code here, to run once:
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  int moveX = random(1,15);
-  int moveY = random(1,15);
+  
+  int moveX = random(1, maxMovementPerTick + 1); // + 1 since Max value is not inclusive
+  int moveY = random(1, maxMovementPerTick + 1);
 
   int flipX = random(0,2);
   if (flipX == 0){
@@ -24,29 +21,10 @@ void loop() {
   if (flipY == 0){
     moveY = moveY * -1;
   }
-
-  x += moveX;
-  y += moveY;
-
-  if (x < 0){
-    x = 0;
-  }
-
-  if (y < 0){
-    y = 0;
-  }
-
-  if (x > 1920){
-    x = 1920;
-  }
-
-  if (y > 1080){
-    y = 1080;
-  }
   
   Serial.println(String(moveX) + " " + String(moveY));
 
-  delay(50);
+  delay(delayMilliseconds);
 
-  //Mouse.move(moveX, moveY);
+  Mouse.move(moveX, moveY);
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Geriatric-Mouse-Cursor-Arduino
 
-This is a small sketch for an arduino leonardo that moves the mouse by a small random offset each loop
+This is a small sketch for an arduino leonardo that moves the mouse by a small random offset each loop that makes the cursor looks shakey. I was playing around with this leonardo and this experiment gave me a good laugh.
